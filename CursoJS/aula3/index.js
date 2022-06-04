ef91adc40f004511654e7662c7055b9ccd785e46
Diego Vespa <diego-vespa2020@outlook.com.br>
2022-06-04 09:32:21 -0300

    att curso JS

---
+++ CursoJS/aula3/index.js
@@ -0,0 +1,8 @@
+console.log('"Diego" Vespa'); //String
+console.log("'Diego' Vespa"); // Number = Número
+console.log(`'Diego'  "Vespa"`);
+
+console.log(35, 12.60, 'Diego Vespa');
+
+// Exercício
+console.log('Meu nome é "Diego Vespa". Estou aprendendo Javascript às', 15, 'da tarde.')
---
+++ CursoJS/aula5/js/index.js
@@ -0,0 +1,3 @@
+console.log('olá mundo!')
+console.log('Este trecho será exibido no console do navegador')
+alert('Olá Mundo!')
