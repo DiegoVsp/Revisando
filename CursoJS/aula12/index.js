511a1b2011cdec642c4e5994ced0c35eae64948c
Diego Vespa <diego-vespa2020@outlook.com.br>
2022-06-07 18:15:52 -0300

    atualizando aulas

--- CursoJS/aula12/index.js
+++ CursoJS/aula12/index.js
@@ -15,3 +15,18 @@ varC = aux;
 console.log(varA,varB,varC)
 
 // [varA,varB,varC] = [varB,varC,varA]; //jeito moderno de realizar o exercício.
+
+// -----------------**---------------
+let umaString = 'O rato roeu a roupa do rei de roma.'
+console.log(umaString.replace('Um','Tres'))
+console.log(umaString.replace(/t/g,'#'))
+
+console.log(umaString.length) // retorna exatamente o tamanho
+console.log(umaString.slice(2,6))
+console.log(umaString.slice(-5,-1))
+// console.log(umaString.substring(umaString.length -5,umaString.length -1))
+
+console.log(umaString.split(' ', 2));
+
+console.log(umaString.toUpperCase());
+console.log(umaString.toLocaleLowerCase());
