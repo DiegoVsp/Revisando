1d58abf12e4ea7537d8e455d575ba843054c9628
Diego Vespa <diego-vespa2020@outlook.com.br>
2022-06-08 17:49:35 -0300

    Arrays revisão

---
+++ CursoJS/aula16/index.js
@@ -0,0 +1,37 @@
+// Arrays
+
+// Arrays são indexados por elementos
+//                 0       1         2
+const alunos = ['Luiz', 'Maria', 'Joao'];
+
+console.log(typeof alunos);
+console.log(alunos instanceof Array)
+// alunos.push('Luiza')
+// alunos.push('Eduardo')
+
+// console.log(alunos.slice(0, 3))
+// console.log(alunos.slice(0, -1))
+// alunos.push('Diego'); //Adiciona no fim
+
+// alunos.unshift('Diego'); // Adiciona no inicio
+// alunos.unshift('Fabio')
+
+console.log(alunos)
+// const removido = alunos.shift(); //remove do inicio
+// const removido = alunos.pop(); //remove do final
+// console.log(removido)
+
+// alunos[0] = 'Eduardo'; //Altera
+// alunos[3] = 'Luiza'; //Adiciona
+
+
+// alunos[alunos.length] = 'Lucca' //Adiciona no fim
+// alunos[alunos.length] = 'Brenda'
+// alunos[alunos.length] = 'Sarah'
+
+console.log(alunos);
+console.log(alunos.length);
+
+// console.log(alunos)
+// console.log(alunos[2])
+
