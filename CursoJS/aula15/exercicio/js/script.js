6ba7df731cd0a58975658c12a786d974865278cf
Diego Vespa <diego-vespa2020@outlook.com.br>
2022-06-08 17:08:45 -0300

    att

--- CursoJS/aula15/exercicio/js/script.js
+++ CursoJS/aula15/exercicio/js/script.js
@@ -5,10 +5,11 @@ const texto = document.getElementById("texto");
 
 
 numeroTitulo.innerHTML = numero;
-texto.innerHTML = `<p>${numero} + 2 é <strong>${numero + 2}</strong></p></br>`
+texto.innerHTML = '';
+texto.innerHTML += `<p>${numero} + 2 é <strong>${numero + 2}</strong></p></br>`
 texto.innerHTML += `<p>Raiz Quadrada: <strong>${numero ** 0.5}</strong></p></br>`
 texto.innerHTML += `<p><strong>${numero} é inteiro? ${Number.isInteger(numero)}</strong> </p></br>`
-texto.innerHTML += `<p>É NaN? <strong>${isNaN(numero)}</strong></p></br>`
+texto.innerHTML += `<p>É NaN? <strong>${Number.isNaN(numero)}</strong></p></br>`
 texto.innerHTML += `<p>Arredondando pra baixo é <strong>${Math.floor(numero)}</strong></p></br>`
 texto.innerHTML += `<p>Arredondando pra cima é <strong>${Math.ceil(numero)}</strong></p></br>`
 texto.innerHTML += `<p>Com duas casas decimais <strong>${numero.toFixed(2)}</strong></p></br>`
