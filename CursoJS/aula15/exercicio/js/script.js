daf6f8231f0351e48e597b92a388056e8b9bb181
Diego Vespa <diego-vespa2020@outlook.com.br>
2022-06-08 10:38:51 -0300

    atualizando aula

---
+++ CursoJS/aula15/exercicio/js/script.js
@@ -0,0 +1,17 @@
+const numero = Number(prompt('Digite um número'));
+// numero = Number(numero)
+const numeroTitulo = document.getElementById("numero-titulo");
+const texto = document.getElementById("texto");
+
+
+numeroTitulo.innerHTML = numero;
+texto.innerHTML = `<p>${numero} + 2 é <strong>${numero + 2}</strong></p></br>`
+texto.innerHTML += `<p>Raiz Quadrada: <strong>${numero ** 0.5}</strong></p></br>`
+texto.innerHTML += `<p><strong>${numero} é inteiro? ${Number.isInteger(numero)}</strong> </p></br>`
+texto.innerHTML += `<p>É NaN? <strong>${isNaN(numero)}</strong></p></br>`
+texto.innerHTML += `<p>Arredondando pra baixo é <strong>${Math.floor(numero)}</strong></p></br>`
+texto.innerHTML += `<p>Arredondando pra cima é <strong>${Math.ceil(numero)}</strong></p></br>`
+texto.innerHTML += `<p>Com duas casas decimais <strong>${numero.toFixed(2)}</strong></p></br>`
+
+
+
---
+++ CursoJS/aula15/index.js
@@ -0,0 +1,27 @@
+console.log(100 / 0);
+
+// let num1 = 25;
+// console.log(num1**(1/2))//pegando raiz quadrada de 9
+// console.log(num1** 0.5)//pegando raiz quadrada de 9
+
+// let num2 = Math.floor(num1)// arredonda para baixo
+// let num2 = Math.ceil(num1)// arredonda para cima
+// let num2 = Math.round(num1)// arredonda para o mais próxima, no caso 9.54 pra cima
+
+// console.log(num2)
+
+// console.log(Math.max(1,2,3,4,5,6,-50,1000))//pega o maior número da sequência
+// console.log(Math.min(1,2,3,4,5,6,-50,1000))//pega o menor número da sequência
+
+// const aleatorio = Math.random()
+// const aleatorio = Math.random()*(10 - 5) + 5; //pega um número aleatório entre 10 e 5
+
+// const aleatorio = Math.round(Math.random()*(10 - 5) + 5); //arredonda números gerados aleatoriamente
+// console.log(aleatorio);
+
+// console.log(Math.pow(2,10)) //potencia
+// console.log(2**10)
+
+// console.log(Math.random()); // gera um número aleatório toda vez que é executado.
+
+
