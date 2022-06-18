818d8f0b4f80e0587c9e997f864c79c68eb08017
Diego Vespa <diego-vespa2020@outlook.com.br>
2022-06-18 14:18:31 -0300

    diferenças var, let e const

---
+++ CursoJS/letVarConst/index.js
@@ -0,0 +1,35 @@
+const verdadeiro = true
+
+// let tem escopo de bloco (...bloco)
+// var só tem escopo de função
+
+// let nome = 'Luiz'; // criando
+// var nome2 = 'Luiz';// criando
+
+
+// var nome2 = 'Otávio'; //redeclarando
+// console.log(nome, nome2)
+
+// if (verdadeiro) {
+//   let nome = 'Otávio' // criando
+//   var nome2 = 'Rogério' //redeclarando
+
+//   // console.log(nome,nome2)
+//   if (verdadeiro) {
+//     var nome2 = 'Ronaldo' //redeclarando
+//     let nome = 'Outra coisa'
+//     // console.log(nome,nome2)
+//   }
+// }
+// console.log(nome, nome2)
+
+
+// function falaOi() {
+//   var sobrenome = 'Miranda'; //dentro do escopo da função
+//   console.log(sobrenome)
+// }
+// falaOi();
+
+console.log(sobrenome)
+
+let sobrenome = 'Miranda'
