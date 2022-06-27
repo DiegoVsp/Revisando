215accad3333e23923c5df97558d7e39ad5332a6
Diego Vespa <diego-vespa2020@outlook.com.br>
2022-06-27 11:21:40 -0300

    att

---
+++ CursoJS/aula34/index.js
@@ -0,0 +1,11 @@
+// for (let i = 1; i <= 10; i ++) {
+//   const par = i % 2 === 0 ? 'É par' : 'É impar'
+//   console.log(` ${i} ${par}`)
+// }; 
+
+const frutas = ['Maçã', 'Pera', 'Uva','Graviola','Acerola']
+// console.log(frutas[1]);
+
+for (let i = 0; i < frutas.length; i++) {
+  console.log(frutas[i])
+}
