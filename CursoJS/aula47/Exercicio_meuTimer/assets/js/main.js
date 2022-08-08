fb8e1b9feaa6ff5e88abbf75b6c791047aa3eafc
DiegoVsp <diego-vespa2020@outlook.com.br>
2022-08-08 12:02:13 -0300

    projeto finalizado

--- CursoJS/aula47/Exercicio_meuTimer/assets/js/main.js
+++ CursoJS/aula47/Exercicio_meuTimer/assets/js/main.js
@@ -5,30 +5,62 @@ const zerar = document.querySelector('.zerar')
 let seconds = 0
 let timer
 
-iniciar.addEventListener('click', (e) => {
-  // alert('Cliquei em iniciar')  
-  // clearInterval(timer)
+function iniciaRelogio() {
   timer = setInterval(() => {
-    relogio.innerHTML = mostraHora(seconds++)
+    relogio.innerHTML = criaHoraDosSegundos(seconds++)
   }, 1000);
-})
-
-pausar.addEventListener('click', (e) => {
-  alert('Cliquei em pausar')
-  setTimeout(() => {
+}
+document.addEventListener('click', (e) => {
+  // console.log(e.target);
+  const elemento = e.target;
+  if (elemento.classList.contains('zerar')) {
+    // console.log('Voce clicou em zerar')
     clearInterval(timer)
-  });
+    relogio.classList.remove('pausado')
+    relogio.innerHTML = '00:00:00'
+    seconds = 0
+  }
+  if (elemento.classList.contains('iniciar')) {
+    // alert('Cliquei em iniciar')  
+    relogio.classList.remove('pausado')
+    clearInterval(timer)
+    iniciaRelogio()
+  }
+  if (elemento.classList.contains('pausar')) {
+    // alert('Cliquei em pausar')
+    setTimeout(() => {
+      clearInterval(timer)
+    });
+    relogio.classList.add('pausado')
+  }
 })
 
-zerar.addEventListener('click', (e) => {
-  alert('Cliquei em zerar')
-  relogio.innerHTML='00:00:00'
-  seconds = 0
-})
+// iniciar.addEventListener('click', (e) => {
+//   // alert('Cliquei em iniciar')  
+//   relogio.classList.remove('pausado')
+//   clearInterval(timer)
+//   iniciaRelogio()
+// })
 
-function mostraHora(seconds) {
-  
+// pausar.addEventListener('click', (e) => {
+//   // alert('Cliquei em pausar')
+//   setTimeout(() => {
+//     clearInterval(timer)
+//   });
+//   relogio.classList.add('pausado')
+// })
+
+// zerar.addEventListener('click', (e) => {
+//   // alert('Cliquei em zerar')
+
+// })
+
+function criaHoraDosSegundos(seconds) {
+
   let data = new Date(seconds * 1000)
-  return data.toLocaleTimeString('pt-BR', { timeZone: 'GMT' }) 
+  return data.toLocaleTimeString('pt-BR', {
+    hour12: false,
+    timeZone: 'GMT'
+  })
 
 }
