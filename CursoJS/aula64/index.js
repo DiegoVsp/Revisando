6e2a352c79ba4f8ce1f065e10a8205fe4349f1e1
DiegoVsp <diego-vespa2020@outlook.com.br>
2022-09-22 11:10:01 -0300

    concatenando array

---
+++ CursoJS/aula64/index.js
@@ -0,0 +1,9 @@
+const a1 = [1, 2, 3, 4];
+const a2 = [5, 6, 7, 8];
+// const a3 = a1.concat(a2)
+
+
+// rest operator ->    ...rest
+// spread operator
+const a3 = [...a1,'Luiz',...a2,...[7,8,9]]
+console.log(a3)
