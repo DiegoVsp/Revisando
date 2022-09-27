7dffaff61dd37ad53a8a0b602ab72ceea021bfad
DiegoVsp <diego-vespa2020@outlook.com.br>
2022-09-27 13:39:33 -0300

    att

---
+++ CursoJS/aula69/index.js
@@ -0,0 +1,12 @@
+const a1 = [10, 20, 30, 40, 50, 60, 70, 80, 90];
+
+// for(let i of a1){
+//   console.log(i)
+// }
+// for(let i in a1){
+//   console.log(a1[i])
+// }
+
+// ForEach
+
+a1.forEach(valor => console.log(valor));
