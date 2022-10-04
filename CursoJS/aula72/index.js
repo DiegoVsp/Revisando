6105518acef10ea88f4b9fa016d93a40a47f5f8e
DiegoVsp <diego-vespa2020@outlook.com.br>
2022-10-04 12:40:06 -0300

    finalizando

---
+++ CursoJS/aula72/index.js
@@ -0,0 +1,30 @@
+// defineProperty - defineProperties
+
+function Produto(nome, preco, estoque) {
+
+
+  Object.defineProperty(this, 'estoque', {
+    enumerable: true, // mostra a chave 
+    value: estoque, // valor da chave
+    writable: true, // controla se pode ou não ser alterado
+    configurable: true //não permite a variavel e não permite reconfigur a variavel
+  });
+  Object.defineProperties(this, {
+    nome: {
+      enumerable: true, // mostra a chave 
+      value: nome, // valor da chave
+      writable: true, // controla se pode ou não ser alterado
+      configurable: true //não permite a variavel e não permite reconfigur a variavel
+    },
+    preco: {
+      enumerable: true, // mostra a chave 
+      value: preco, // valor da chave
+      writable: true, // controla se pode ou não ser alterado
+      configurable: true //não permite a variavel e não permite reconfigur a variavel
+    }
+  })
+}
+
+const produto1 = new Produto('Sabão', 12.00, 99)
+
+console.log(produto1)
