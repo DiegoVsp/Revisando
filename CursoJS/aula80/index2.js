fa6086d37780bd5b9407c4d27230f7c04b1817b1
DiegoVsp <diego-vespa2020@outlook.com.br>
2022-10-24 10:11:30 -0300

    att

--- CursoJS/aula80/index2.js
+++ CursoJS/aula80/index2.js
@@ -1,12 +1,13 @@
 // Getters e Setters Classe
 const _velocidade = Symbol(); // gera um Id aleatório
+// 
 
 class Carro {
   constructor(nome) {
     this.nome = nome
     this[_velocidade] = 0; //utilizando a variável(não precisa do ponto)
   }
-  set velocidade(valor) {
+  set velocidade(valor) { 
     console.log('SETTER')
     if (typeof valor !== 'number') return;
     if (valor >= 100 || valor <= 0) return;
@@ -32,6 +33,7 @@ const c1 = new Carro('Fusca')
 // }
 c1.velocidade = 55
 // console.log(c1.velocidade)
+console.log(c1[_velocidade])
 
 console.log('')
 console.log('--------PESSOA---------')
@@ -52,5 +54,5 @@ class Pessoa {
   }
 }
 const p1 = new Pessoa('Diego', 'Vespa')
-p1.nomeCompleto = 'Luiz Miranda'
-console.log(p1)
+p1.nomeCompleto = 'Luiz Miranda' // Setter 
+console.log(p1.nomeCompleto)
