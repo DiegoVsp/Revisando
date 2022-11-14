e5d02a0d46c0433a8efefa59d7561b016be5896e
DiegoVsp <diego-vespa2020@outlook.com.br>
2022-11-14 09:35:02 -0300

    att

--- Classes/index.js
+++ Classes/index.js
@@ -91,3 +91,48 @@ let pessoa3 = new Pessoa3('Diego', 'Vespa')
 console.log(pessoa3.nomePcompleto)
 pessoa3.nomePcompleto = 'Francisca Silva'
 console.log(pessoa3)
+
+console.log()
+console.log()
+console.log('Herança com Classes')
+
+class DispositivoEletronico {
+  constructor(nome,) {
+    this.nome = nome;
+    this.ligado = false;
+  }
+  ligar() {
+    if (this.ligado) {
+      console.log(this.nome + ' está Ligado')
+      return
+    }
+    this.ligado = true;
+  }
+  desligado() {
+    if (!this.ligado) {
+      console.log(this.nome + ' já está desligado')
+      return
+    }
+    this.ligado = false;
+
+  }
+}
+
+
+let monitor1 = new DispositivoEletronico('monitor')
+monitor1.ligar()
+monitor1.desligado()
+monitor1.desligado()
+// monitor1.ligar()
+console.log(monitor1)
+console.log(monitor1.modelo)
+
+class Smartfone extends DispositivoEletronico {
+  constructor(nome, cor, modelo) {
+    super(nome);
+    this.cor = cor;
+    this.modelo = modelo
+  }
+}
+let s1 = new Smartfone('Samsung', 'Preto', 'Galaxy S10')
+console.log(s1)
