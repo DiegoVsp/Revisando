012bc9c8ec4b3894ca4518c381a2f8342aa931e3
Diego Vespa <diego-vespa2020@outlook.com.br>
2022-12-14 10:48:35 -0300

    pegando nome usuario usando Fetch API

--- Promises/fetchAPI/index.js
+++ Promises/fetchAPI/index.js
@@ -1,4 +1,6 @@
 const userData = document.querySelector('[user-js="user-data"]')
+
+
 fetch('https://jsonplaceholder.typicode.com/users')
   .then((response) => {
     if (!response.ok) {
@@ -7,8 +9,38 @@ fetch('https://jsonplaceholder.typicode.com/users')
     return response.json()
   })
   .then((users) => {
-    console.log(users)
+    for(let user of users){
+    // console.log(user.username)
+    let li = document.createElement('li');
+    let span = document.createElement('span');
+    span.innerText = user.username;
+    li.appendChild(span);
+    // li.innerHTML = `${user.username}`
+    userData.appendChild(li)
+    }
   })
   .catch((error) => {
     console.log(error)
   })
+
+
+// const userData = document.querySelector('[user-js="user-data"')
+// // console.log(userData
+
+// const url = 'https://randomuser.me/api/?results=10'
+// fetch(url)
+//   .then(response => {
+//     return response.json()
+//   })
+//   .then((user) => {
+//     const authors = user.results
+//     authors.map(author => {
+//       const li = document.createElement('li')
+//       const span = document.createElement('span')
+//       span.innerText = author.name.first
+//       li.appendChild(span)
+//       userData.appendChild(li)
+//     })
+//     return authors
+//   })
+
